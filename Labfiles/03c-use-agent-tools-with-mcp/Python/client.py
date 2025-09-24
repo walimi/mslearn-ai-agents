7716acaf21d1e8622926627ee1da096fbd2b1af7
Wahid Alimi <wahid.alimi@gmail.com>
2025-09-24 09:29:58 -0400

    Added logic foragent that uses the Microsoft Learn MCP server

--- Labfiles/03c-use-agent-tools-with-mcp/Python/client.py
+++ Labfiles/03c-use-agent-tools-with-mcp/Python/client.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 
 # Add references
-
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents import AgentsClient
+from azure.ai.agents.models import McpTool, ToolSet, ListSortOrder
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,19 +12,45 @@ project_endpoint = os.getenv("PROJECT_ENDPOINT")
 model_deployment = os.getenv("MODEL_DEPLOYMENT_NAME")
 
 # Connect to the agents client
-
+agents_client = AgentsClient(
+    endpoint=project_endpoint,
+    credential=DefaultAzureCredential(
+        exclude_environment_credential=True,
+        exclude_managed_idetity_credential=True
+    )
+)
 
 # MCP server configuration
 mcp_server_url = "https://learn.microsoft.com/api/mcp"
 mcp_server_label = "mslearn"
 
 # Initialize agent MCP tool
+mcp_tool = McpTool(
+    server_label=mcp_server_label,
+    server_url=mcp_server_url
+)
+
+mcp_tool.set_approval_mode("never")
+
+toolset = ToolSet()
+toolset.add(mcp_tool)
+
 
 
 # Create agent with MCP tool and process agent run
 with agents_client:
 
     # Create a new agent
+    agent = agents_client.create_agent(
+        model=model_deployment,
+        name="my-mcp-agent",
+        description="This agent uses the MCP server at https://learn.microsoft.com/api/mcp to find the latest official Microsoft documentation.",
+        instructions="""
+        You have access to an MCP server called `microsoft.docs.mcp` - this tool allows you to 
+        search through Microsoft's latest official documentation. Use the available MCP tools
+        to answer questions and perform tasks."""
+    )
+    
     
 
     # Log info
@@ -30,12 +58,21 @@ with agents_client:
     print(f"MCP Server: {mcp_tool.server_label} at {mcp_tool.server_url}")
 
     # Create thread for communication
-    
+    thread = agents_client.threads.create()
+    print(f"Crated thread, ID: {thread.id}")    
 
     # Create a message on the thread
-    
+    prompt = input("\nHow can I help?: ")
+    message = agents_client.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=prompt,
+    )
+    print(f"Created message, ID: {message.id}")
 
     # Create and process agent run in thread with MCP tools
+    run = agents_client.runs.create_and_process(thread_id=thread.id, agent_id=agent.id, toolset=toolset)
+    print(f"Created run, ID: {run.id}")
     
     
     # Check run status
@@ -73,5 +110,5 @@ with agents_client:
             print("-" * 50)
 
     # Clean-up and delete the agent once the run is finished.
-    agents_client.delete_agent(agent.id)
-    print("Deleted agent")
+    # agents_client.delete_agent(agent.id)
+    # print("Deleted agent")
